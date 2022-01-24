5992b6a50f26e6c948a3c30fa7ce3bd53008b872
Reese Kuper <35940701+rkuper@users.noreply.github.com>
2022-01-23 23:21:17 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -56,7 +56,7 @@ for BENCHMARK in "${BENCHMARKS[@]}"; do
 
       ycsb)
         # Clone
-        git clone https://github.com/brianfrankcooper/YCSB.git ${BENCHMARK}
+        git clone https://github.com/rkuper/YCSB.git ${BENCHMARK}
 
         # Dependencies
         sudo apt install -y redis memcached libmemcached-dev
